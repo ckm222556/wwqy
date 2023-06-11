848ac993cbd4b6f698b73afb4c4cb66fb831bef3
ckm222556 <49216338+ckm222556@users.noreply.github.com>
2023-06-11 13:00:10 +0800

    1

--- js/jump.js
+++ js/jump.js
@@ -1,12 +1,35 @@
 function jump() {
-        window.location.href="https://val.qq.com/";
-}
-        
-
-function sleep(n) {
-    var start = new Date().getTime();
-    b = true;
-    while (b) {
-        b= (new Date().getTime() - start) < n;
+    var time = new Date().getTime();
+    time = time % 10;
+    
+    switch (time) {
+        case 0: jumpCsgo();
+            break;
+        case 1 :jumpYuan();
+            break;
+        default: jumpDefault();
     }
+}
+
+function jumpCsgo(){
+    document.getElementById("yuan").style.textDecoration="line-through"
+    document.getElementById("wwqy").style.textDecoration="line-through"
+    var myTarget = window.setTimeout(function(){
+        window.location.href = "https://www.csgo.com.cn/";}, 1000
+    );
+    
+}
+function jumpYuan(){
+    document.getElementById("csgo").style.textDecoration="line-through"
+    document.getElementById("wwqy").style.textDecoration="line-through"
+    var myTarget = window.setTimeout(function(){
+        window.location.href = "https://ys.mihoyo.com/main/";}, 1000
+    );
+}
+function jumpDefault(){
+    document.getElementById("csgo").style.textDecoration="line-through"
+    document.getElementById("yuan").style.textDecoration="line-through"
+    var myTarget = window.setTimeout(function(){
+        window.location.href = "https://val.qq.com/";}, 1000
+    );
 }
